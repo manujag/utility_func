806c4df773b54b58431e3ad0e2c4a2f58c0b9336
manujag <mjmanu91@gmail.com>
2022-03-17 16:01:07 +0530

    added sftp file transfer using private key

---
+++ sftp_private_key_login.py
@@ -0,0 +1,29 @@
+import paramiko
+import os
+
+sftp_host = 'sftp_host_name'
+sftp_user = ''  # username
+pass_phrase = ''
+pkey_file_path = ''
+target_dir = r'/public/'
+src_dir = ''
+src_file = 'test.txt'
+tgt_file = src_file
+
+
+# create an ssh cleint for the session
+session = paramiko.SSHClient()
+
+session.load_system_host_keys()
+# get the key from private key file
+sftp_pkey = paramiko.RSAKey.from_private_key_file(
+    filename=pkey_file_path, password=pass_phrase)
+
+# connect to the ssh client
+session.connect(hostname=sftp_host, port=22, pkeys=sftp_pkey,
+                allow_agent=False, look_for_keys=False)
+
+# estabalish sftp connection and upload the file to required folder
+with session.open_sftp() as ftp_client:
+    ftp_client.put(localpath=os.path.join(src_dir, src_file),
+                   remotepath=target_dir + r'/' + tgt_file, confirm=True)
